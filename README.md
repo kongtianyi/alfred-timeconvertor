b26391b9d77eb76ce70dd6622a5999e8afd393bf
kongtianyi <kongtianyi@foxmail.com>
2022-12-11 02:55:01 +0800

    feat: adjust input pattern

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
-# alfred-timestamp
-A alfred workflow to deal with timestamp convert, including i18n features.
+# alfred-timeconvertor
+An alfred workflow that do time convert, including i18n features.
 
-Workflow file are in the repo's root derictory, named `Timestamp I18N.alfredworkflow`.
+Workflow file is in the repo's root derictory, named `Time Convertor.alfredworkflow`. You can just download it on your mac and double click to install.
 
-## Input Format:
+## Input Format
 ```
-ts [timezone: utf8] (now|timestamp|datetime)
+ts (now|<timestamp>|<datetime>[#<timezone>]) [<timezone>]...
+# timestamp support second and milisecond
+# datetime's format is yyyy-MM-dd hh:mm:ss
+# timezone's format is utc[\+|\-][0-9]+(utc, utc8, utc-12 etc.)
 ```
 
-## Examples(WIP)
-
+## Examples
+1. get local timestamp and datetime for now
+ ![image](https://user-images.githubusercontent.com/15275771/206860949-a660ca09-b430-4286-b646-82d93b29268a.png)
+2. convert timestamp to local datetime
+ ![image](https://user-images.githubusercontent.com/15275771/206861015-c44dfd97-9cc3-419b-84c7-f63d81855db3.png)
+3. convert local datetime to timestamp
+ ![image](https://user-images.githubusercontent.com/15275771/206861044-53d960c9-16ac-4a4e-9d82-79249ee89f71.png)
 
 ## Appendix
 
--- timestamp.py
+++ timestamp.py
@@ -5,36 +5,83 @@ import os
 import sys
 import pytz
 import time
+from enum import Enum
 from workflow import Workflow3
 from datetime import datetime
+
+LOCAL_TIME = "local time"
+TIMESTAMP = "timestamp"
+
+
+class QueryType(Enum):
+    INVALID = "invalid"
+    NOW = "now"
+    TIMESTAMP = "timestamp"
+    DATETIME = "datetime"
+
+
+class MyTimezone:
+    def __init__(self, offset, is_local=False):
+        """
+        init
+        :param offset: offset from utc timezone, unit: hour
+        """
+        self.offset = offset
+        self.timezone = pytz.FixedOffset(offset * 60)
+        if offset == 0:
+            self.name = "UTC"
+        elif offset > 0:
+            self.name = "UTC+" + str(offset)
+        else:
+            self.name = "UTC" + str(offset)
+        self.is_local = is_local
+
+    def get_offset_second(self):
+        return self.offset * 3600
 
 
 def main(wf):
     query = wf.args[0]
-    query_time, _timezone, timezone_offset = parse_input(query)
+    query_type, query_subject, timezone_list = parse_input(query)
 
-    if query_time == 'now':
+    if query_type == QueryType.NOW:
         now_timestamp = int(time.time())
-        append_timestamp_result(wf, now_timestamp)
-        time_result = format_timestamp(now_timestamp, _timezone)
-        append_datetime_result(wf, time_result)
-    elif is_timestamp(query_time):
-        query_timestamp = parse_timestamp(query_time)
-        time_result = format_timestamp(query_timestamp, _timezone)
-        append_datetime_result(wf, time_result)
-    else:
-        if ":" in query_time:
-            time_data = datetime.strptime(query_time, "%Y-%m-%d %H:%M:%S")
+        add_item(wf, TIMESTAMP, now_timestamp)
+        time_result = format_timestamp(now_timestamp)
+        add_item(wf, LOCAL_TIME, time_result)
+        for _timezone in timezone_list:
+            time_result = format_timestamp(now_timestamp, _timezone.timezone)
+            add_item(wf, _timezone.name, time_result)
+    elif query_type == QueryType.TIMESTAMP:
+        query_timestamp = parse_timestamp(query_subject)
+        time_result = format_timestamp(query_timestamp)
+        add_item(wf, LOCAL_TIME, time_result)
+        for _timezone in timezone_list:
+            time_result = format_timestamp(query_timestamp, _timezone.timezone)
+            add_item(wf, _timezone.name, time_result)
+    elif query_type == QueryType.DATETIME:
+        sharp_index = query_subject.find("#")
+        if sharp_index != -1:
+            query_datetime = query_subject[0: sharp_index]
+            from_timezone = MyTimezone(get_utc_offset_hour(query_subject[sharp_index + 1:]))
         else:
-            time_data = datetime.strptime(query_time, "%Y-%m-%d")
-        time_result = int(convert_datetime_to_timestamp_with_timezone_offset(time_data, timezone_offset))
-        append_timestamp_result(wf, time_result)
+            query_datetime = query_subject
+            from_timezone = MyTimezone(get_local_offset_hour_from_utc(), is_local=True)
+        time_data = datetime.strptime(query_datetime, "%Y-%m-%d %H:%M:%S")
+        timestamp = int(
+            convert_datetime_to_timestamp_with_timezone_offset(time_data, from_timezone.get_offset_second()))
+        append_timestamp_result(wf, timestamp)
+        for _timezone in timezone_list:
+            time_result = format_timestamp(timestamp, _timezone.timezone)
+            add_item(wf, _timezone.name, time_result)
+    else:
+        add_item(wf, "unknown pattern", query)
     wf.send_feedback()
 
 
-def append_datetime_result(wf, time_result):
-    title = 'datetime: %s' % (time_result,)
-    wf.add_item(title=title, arg=time_result, valid=True)
+def add_item(wf, prefix, result):
+    title = '{}: {}'.format(prefix, result)
+    wf.add_item(title=title, arg=result, valid=True)
 
 
 def append_timestamp_result(wf, timestamp):
@@ -44,24 +91,42 @@ def append_timestamp_result(wf, timestamp):
 
 def parse_input(key):
     input = key.strip()
-    params = input.split(' ', 1)
-    is_set_timezone = len(params) == 2 and params[0].startswith('utc')
-    if is_set_timezone:
-        aimed_timezone_str = params[0]
-        aimed_timezone_offset = 0
-        if len(aimed_timezone_str) > 3:
-            aimed_timezone_offset = int(aimed_timezone_str[3:])
-        used_timezone = pytz.FixedOffset(aimed_timezone_offset * 60)
-        timezone_offset = aimed_timezone_offset * 60 * 60
-        query_time = params[1]
+    params = input.split(' ')
+    if params[0] == "now":
+        query_type = QueryType.NOW
+        query_subject = "now"
+        tzs = params[1:]
+    elif is_timestamp(params[0]):
+        query_type = QueryType.TIMESTAMP
+        query_subject = params[0]
+        tzs = params[1:]
+    elif is_datetime(params):
+        query_type = QueryType.DATETIME
+        query_subject = " ".join([params[0], params[1]])
+        tzs = params[2:]
     else:
-        used_timezone = None
-        query_time = input
-        timezone_offset = get_local_offset_from_utc()
-    return query_time, used_timezone, timezone_offset
+        query_type = QueryType.INVALID
+        query_subject = ""
+        tzs = []
+
+    aimed_timezone_list = list()
+    for tz in tzs:
+        tz = tz.strip()
+        if tz.find('#') != -1:
+            continue
+        if tz.find('utc') == -1:
+            continue
+        offset_hour = get_utc_offset_hour(tz)
+        aimed_timezone_list.append(MyTimezone(offset_hour))
+
+    return query_type, query_subject, aimed_timezone_list
 
 
-def format_timestamp(timestamp, _timezone):
+def get_utc_offset_hour(tz):
+    return int(tz[3:]) if len(tz) > 3 else 0
+
+
+def format_timestamp(timestamp, _timezone=None):
     return datetime.fromtimestamp(timestamp, tz=_timezone).strftime("%Y-%m-%d %H:%M:%S")
 
 
@@ -82,6 +147,20 @@ def is_timestamp(value):
     return True
 
 
+def is_datetime(params):
+    if len(params) < 2:
+        return False
+    dt = " ".join([params[0], params[1]])
+    sharp_index = dt.find('#')
+    if sharp_index != -1:
+        dt = dt[:sharp_index]
+    try:
+        datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
+    except:
+        return False
+    return True
+
+
 def parse_timestamp(value):
     data = int(value)
     strlen = len(value)
@@ -97,6 +176,11 @@ def convert_datetime_to_timestamp_with_timezone_offset(_datetime, offset_second)
 def get_local_offset_from_utc():
     now = time.time()
     return (datetime.fromtimestamp(now) - datetime.utcfromtimestamp(now)).total_seconds()
+
+
+def get_local_offset_hour_from_utc():
+    second = get_local_offset_from_utc()
+    return int(second / 3600)
 
 
 if __name__ == '__main__':
